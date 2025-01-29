552f46359c6201c54ca07456f365bc1ef0691d66
Rodrigo Oliveira <rodrigo.silva.olviera369@gmail.com>
2025-01-29 00:57:05 -0300

    Valida a request de Registrar

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,4 +1,5 @@
-﻿using CashFlow.Communication.Requests;
+﻿using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
 
 namespace CashFlow.Application.UseCases.Expenses.Register;
@@ -7,8 +8,34 @@ public class RegisterExpenseUseCase
 {
     public ResponseRegisterExpenseJson Execute(RequestRegisterExpenseJson request)
     {
-        //To Do Validations
+        Validate(request);
         
         return new ResponseRegisterExpenseJson();
+    }
+
+    private void Validate(RequestRegisterExpenseJson request)
+    {
+        var titleIsEmpty = string.IsNullOrWhiteSpace(request.Title);
+        if (titleIsEmpty)
+        {
+            throw new ArgumentException("The title is required");
+        }
+
+        if (request.Amount <= 0)
+        {
+            throw new ArgumentException("The amount must be greater than 0");
+        }
+
+        var validateDate = DateTime.Compare(request.Date, DateTime.UtcNow);
+        if (validateDate > 0)
+        {
+            throw new ArgumentException("The date can not be greater than the current date");
+        }
+
+        var paymentTypeIsValid = Enum.IsDefined(typeof(PaymentType), request.PaymentType);
+        if (!paymentTypeIsValid)
+        {
+            throw new ArgumentException("The payment type is not valid");
+        }
     }
 }
