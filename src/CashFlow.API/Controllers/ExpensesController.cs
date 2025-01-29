e216e6eb23b8c59c8100695215ca0cb61c5009aa
Rodrigo Oliveira <rodrigo.silva.olviera369@gmail.com>
2025-01-29 12:59:57 -0300

    Retorna um erro em formato json e cria uma response de erros

--- src/CashFlow.API/Controllers/ExpensesController.cs
+++ src/CashFlow.API/Controllers/ExpensesController.cs
@@ -1,20 +1,33 @@
 using CashFlow.Application.UseCases.Expenses.Register;
 using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace CashFlow.API.Controllers
+namespace CashFlow.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ExpensesController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ExpensesController : ControllerBase
+    [HttpPost]
+    public IActionResult Register([FromBody] RequestRegisterExpenseJson request)
     {
-        [HttpPost]
-        public IActionResult Register([FromBody] RequestRegisterExpenseJson request)
+        try
         {
             var useCase = new RegisterExpenseUseCase();
             var response = useCase.Execute(request);
             return Created(String.Empty, response);
+        }
+        catch (ArgumentException ex)
+        {
+            var errorResponse = new ResponseErrorJson(ex.Message);
+            return BadRequest(errorResponse);
+        }
+        catch
+        {
+            var errorResponse = new ResponseErrorJson("unknown error");
+            return StatusCode(StatusCodes.Status500InternalServerError, errorResponse);
         }
     }
 }
---
+++ src/CashFlow.Communication/Responses/ResponseErrorJson.cs
@@ -0,0 +1,6 @@
+﻿namespace CashFlow.Communication.Responses;
+
+public class ResponseErrorJson(string errorMessage)
+{
+    public string ErrorMessage { get; set; } = errorMessage;
+}
