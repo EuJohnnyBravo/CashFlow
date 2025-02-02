8719644c49993334183f2ff260e3c6c0a3c6eb94
Rodrigo Oliveira <rodrigo.silva.olviera369@gmail.com>
2025-02-02 00:11:59 -0300

    cria o endpoint getbyid e suas regras de negocio e tratativas de erro

---
+++ src/CashFlow.Application/UseCases/Expenses/GetById/GetExpenseByIdUseCase.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using CashFlow.Communication.Responses;
+using CashFlow.Domain.Repositories.Expenses;
+using CashFlow.Exception;
+using CashFlow.Exception.ExceptionsBase;
+
+namespace CashFlow.Application.UseCases.Expenses.GetById;
+
+public class GetExpenseByIdUseCase(
+    IExpensesRepository repository,
+    IMapper mapper
+    ): IGetExpenseByIdUseCase
+{
+    public async Task<ResponseExpenseJson> Execute(long id)
+    {
+        var result = await repository.GetById(id);
+        if (result is null)
+        {
+            throw new NotFoundException(ResourceErrorMessages.EXPENSE_NOT_FOUND);
+        }
+        return mapper.Map<ResponseExpenseJson>(result);
+    }
+}
